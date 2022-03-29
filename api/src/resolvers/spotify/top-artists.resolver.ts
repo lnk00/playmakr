c19fa85ccea358e644509783ea48492f0c65c53c
Damien Dumontet <damien.dumontet@protonmail.com>
2022-03-29 12:57:05 +0200

    fix typo

--- api/src/resolvers/spotify/top-artists.resolver.ts
+++ api/src/resolvers/spotify/top-artists.resolver.ts
@@ -3,14 +3,14 @@ import { get } from '../../tools/fetcher.tools';
 
 export default async (args: any): Promise<Artist[]> => {
   const res = await get(`${import.meta.env.VITE_SPOTIFY_BASE_URL}me/top/artists`, args.token);
-  const tracks: Artist[] = [];
+  const artists: Artist[] = [];
   res?.data.items.forEach((element: any, idx: number) => {
-    tracks.push({
+    artists.push({
       rank: idx + 1,
       name: element.name,
       image: element.images[1].url,
       followers: element.followers.total,
     });
   });
-  return tracks;
+  return artists;
 };
